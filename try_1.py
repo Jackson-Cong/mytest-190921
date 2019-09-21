e3de45a0bcba386267bb80640009719bbe6da19c
Jackson-Cong <1315187655@qq.com>
2019-09-21 15:19:56 +0800

    +-*/

---
+++ try_1.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+'''
+for num in range(5):
+for num in range(1,5):
+for num in range(1,5,2):
+
+'''
+num_start = 1
+num_end = 1000000000
+
+'''
+num_input = input ('type an end number:')
+num-end = int (num_input)
+'''
+
+sum = 0
+# 自然数求和
+for num in range(num_start, num_end + 1):
+    sum += num
+
+print (sum)
---
+++ try_2.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+# sum of natural number
+
+import fractions
+
+num_start = 1
+num_end = 5
+
+sum = 0
+for num in range (num_start, num_end + 1):
+    #fc = 1 / num
+    fc = fractions.Fraction(1, num)
+    sum += fc
+    print('add %s to summaary, then sum=%s' %(fc, sum) )
+
+print(sum)
